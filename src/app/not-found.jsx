438983fa5bf0f4d5e167ff1624c26b40e669df03
Vabdala2003 <viniciusmta2003@gmail.com>
2023-10-17 22:48:14 -0300

    not-found

---
+++ src/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1>OOPS! 404</h1>
+      <p>Not Found</p>
+      <div>
+      </div>
+    </div>
+  )
+}
