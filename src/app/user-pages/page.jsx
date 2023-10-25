52a576095fd4fedf900851942e9172366d0c921d
vabdala2003 <vinciusmta2003@gmail.com>
2023-10-24 22:52:26 -0300

    update

--- src/app/user-pages/page.jsx
+++ src/app/user-pages/page.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
 import Image from 'next/image';
+import { useEffect } from 'react';
 
 export default function UserPages() {
 
+  useEffect(() => {
+    fetch("https://locaL:3000/api/git-users")
+    .then((response)=> response.json())
+    .then((response)=> setUsers(response))
+    .catch(errors=> console.error(errors));
+
+  }, [])
+
   const response = await fetch("/api/git-users");
-  if (response.ok) {
-    return response.json();
-  }
-  else {
-    throw new Error("Error getting users from API")
-  }
+  const users = await response.json();
+
+    console.log(users)
 
   return (
     <div>
@@ -18,17 +24,18 @@ export default function UserPages() {
       <div>
         <ul>
           {
-            response.map(user)=>(
+            response.map((user)=>(
               <li key={user.id}>
                 <figure>
-                  <
+                  <Image
                   src={user.avatar_url}
                   alt="Avatar"
                   Width={50}
                   Height={50}/>
+                  <figcaption>{user.login}</figcaption>
                 </figure>
               </li>
-            )
+            ))
           }
         </ul>
       </div>
